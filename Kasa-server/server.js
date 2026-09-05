bcd8c7848f43fb9c7a9d09ac7de6839d7ec8a59e
jggowthaman <jgowthaman56@gmail.com>
2026-09-05 20:14:43 +0530

    Update backend CORS configuration

--- Kasa-server/server.js
+++ Kasa-server/server.js
@@ -30,7 +30,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+   origin: [
+  "http://localhost:5173",
+  "https://kasa-luxe.com",
+  "https://www.kasa-luxe.com",
+],
     credentials: true,
   })
 );
